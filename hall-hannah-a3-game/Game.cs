6219b9c2659d72f0edcccb9395d564878ddd2ddb
Hannahh99 <000972499@mohawkcollege.ca>
2025-11-12 12:32:49 -0500

    Add base background Add base score

---
+++ hall-hannah-a3-game/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MohawkGame2D
+{
+    public class Game
+    {
+        // Place your variables here:
+
+        public void Setup()
+        {
+            Window.SetTitle("Game");
+            Window.SetSize(400, 400);
+        }
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+
+            BackgroundGeneration();
+            Score();
+        }
+
+        public void BackgroundGeneration()
+        {
+            // Ground
+            Draw.FillColor = Color.Black;
+            Draw.LineColor = Color.Black;
+            Draw.LineSize = 1;
+            Draw.Rectangle(0, 240, 400, 2);
+
+            // Obstacles
+
+        }
+
+        public void Score()
+        {
+            // Display Score
+            string scoreText = $"{Time.SecondsElapsed:0.00}";
+            Text.Color = Color.Black;
+            Text.Draw(scoreText, 10, 10);
+        }
+    }
+
+}
---
+++ hall-hannah-a3-game/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hall_hannah_a3_game.MohawkGame2D.Classes
+{
+    internal class Player
+    {
+    }
+}
